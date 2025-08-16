c6e6ca74446db24d00849d6e039984989e35707f
Ana Bárbara <aninhabarbara1997@hotmail.com>
2025-08-16 13:58:36 -0300

    Update jogonumero.js

--- jogonumero.js
+++ jogonumero.js
@@ -19,23 +19,22 @@ if (chute === numeroSecreto) {
 
     // cria confete
     for (let i = 0; i < 100; i++) {
-        const confete = document.createElement("div");
-        confete.className = "confete";
-        confete.style.left = Math.random() * window.innerWidth + "px";
-        confete.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
-        document.body.appendChild(confete);
+    const confete = document.createElement("div");
+    confete.className = "confete";
+    confete.style.left = "50%"; // explosão do centro
+    confete.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
 
-        setTimeout(() => confete.remove(), 3000); // remove depois de 3s
-        }
+    // posição final aleatória
+    const x = (Math.random() - 0.5) * 500 + "px"; // para esquerda/direita
+    const y = Math.random() * 500 + "px"; // para baixo
+    confete.style.setProperty("--x", x);
+    confete.style.setProperty("--y", y);
 
-        // gera um novo número secreto para a próxima rodada
-        numeroSecreto = gerarNumeroSecreto();
-    } else if (chute > numeroSecreto) {
-        mensagem.textContent = "O número secreto é menor!";
-    } else {
-        mensagem.textContent = "O número secreto é maior!";
-    }
+    document.body.appendChild(confete);
+    setTimeout(() => confete.remove(), 3000);
 }
+
+
 
 
 
